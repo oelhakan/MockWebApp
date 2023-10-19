c91bb5b690118f968254308efa1a9e8cbb727b07
Onur Elhakan <72891423+oelhakan@users.noreply.github.com>
2023-10-19 22:57:25 +0200

    first commit

---
+++ src/test/java/pl/edu/pwr/mockwebapp/MockWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.mockwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
